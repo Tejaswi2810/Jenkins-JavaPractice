cfa3d5e3910ad0271e576a33264197a4293e77da
Tejaswi2810 <87350768+Tejaswi2810@users.noreply.github.com>
2025-06-02 13:24:07 -0400

    Update App.java

--- App.java
+++ App.java
@@ -4,5 +4,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins + Maven + Java!");
         System.out.println("Pushing");
+        System.out.println("Pushing for the second time to check if the jar file is generated");
+        
+        
     }
 }
